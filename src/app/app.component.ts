fbd174c7b42723194f1489b7b51414079bbcfe06
Ethan Strain <ethanmstrain@gmail.com>
2024-05-19 16:17:07 -0600

    working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  async ngOnInit() {
+    const response = await fetch('https://wmzeeupjd57owgpb2hjeciiq5e0huipa.lambda-url.us-east-2.on.aws/payroll/',{
+      method: 'GET'
+    })
+    const result = await response.json();
+    console.log(result)
+    this.PastData = result;
+  }
   Result!: string;
+  PastData!: any;
   async NewQuestion(){
     let Question = (<HTMLInputElement>document.getElementById('header')).value;
     let Answer = (<HTMLInputElement>document.getElementById('description')).value;
@@ -25,7 +34,7 @@ export class AppComponent {
       body: JSON.stringify(send)
     })
     this.Result = await response.status.toString();
-    
+
     if(this.Result == '200'){
       (<HTMLInputElement>document.getElementById('header')).value = '';
       (<HTMLInputElement>document.getElementById('description')).value = '';
