64230f72fea352a92b765f525e3fe32901f6ee30
estrai3@wgu.edu <estrai3@wgu.edu>
2024-05-19 14:30:34 -0600

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'QuestionAPI';
+  Result!: string;
+  async NewQuestion(){
+    let Question = (<HTMLInputElement>document.getElementById('header')).value;
+    let Answer = (<HTMLInputElement>document.getElementById('description')).value;
+    let KeyWords = (<HTMLInputElement>document.getElementById('tags')).value;
+    let Category = (<HTMLInputElement>document.getElementById('category')).value;
+
+    let send = {
+      category: Question,
+      description: Answer,
+      header: KeyWords,
+      tags: Category
+    }
+    const response = await fetch('https://wmzeeupjd57owgpb2hjeciiq5e0huipa.lambda-url.us-east-2.on.aws/new-question/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(send)
+    })
+    this.Result = await response.status.toString();
+    
+    if(this.Result == '200'){
+      (<HTMLInputElement>document.getElementById('header')).value = '';
+      (<HTMLInputElement>document.getElementById('description')).value = '';
+      (<HTMLInputElement>document.getElementById('tags')).value = '';
+      (<HTMLInputElement>document.getElementById('category')).value = 'Payroll';
+    }
+    setTimeout(() => {
+      this.Result = ''
+    }, 1500);
+  }
 }
